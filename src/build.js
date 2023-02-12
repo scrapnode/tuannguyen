5cb59252cb9d3c4a7640607e22d3c0dc0e3126d1
Tuan Nguyen <tuan.nguyen930708@gmail.com>
2023-02-12 11:43:46 +0700

    ci(2023.212.1143): ✨🐛🚨

--- src/build.js
+++ src/build.js
@@ -1,62 +1,70 @@
-const handlebars = require('handlebars');
-const fs = require('fs-extra');
-const markdownHelper = require('./utils/helpers/markdown');
-const templateData = require('./metadata/metadata');
-const Puppeteer = require('puppeteer');
-const getSlug = require('speakingurl');
-const dayjs = require('dayjs');
+const handlebars = require("handlebars");
+const fs = require("fs-extra");
+const markdownHelper = require("./utils/helpers/markdown");
+const templateData = require("./metadata/metadata");
+const Puppeteer = require("puppeteer");
+const getSlug = require("speakingurl");
+const dayjs = require("dayjs");
 
 const srcDir = __dirname;
-const outputDir = __dirname + '/../dist';
+const outputDir = __dirname + "/../dist";
 
 // Clear dist dir
 fs.emptyDirSync(outputDir);
 
 // Copy assets
-fs.copySync(srcDir + '/assets', outputDir);
+fs.copySync(srcDir + "/assets", outputDir);
 // Build HTML
-handlebars.registerHelper('markdown', markdownHelper);
-const source = fs.readFileSync(srcDir + '/templates/index.html', 'utf-8');
+handlebars.registerHelper("markdown", markdownHelper);
+const source = fs.readFileSync(srcDir + "/templates/index.html", "utf-8");
 const template = handlebars.compile(source);
-const pdfFileName = `${getSlug(templateData.name)}.${getSlug(templateData.title)}.pdf`;
+const pdfFileName = `${getSlug(templateData.name)}.${getSlug(
+  templateData.title
+)}.pdf`;
 const html = template({
   ...templateData,
   pdfFileName,
-  updated: dayjs().format('MMMM D, YYYY'),
+  updated: dayjs().format("MMMM D, YYYY"),
 });
-fs.writeFileSync(outputDir + '/index.html', html);
+fs.writeFileSync(outputDir + "/index.html", html);
 
+let sync = true;
 buildPdf = async function (inputFile, outputFile) {
   const browser = await Puppeteer.launch({
     headless: true,
-    slowMo: 50
+    slowMo: 50,
   });
-  console.log("---1")
+  console.log("---1");
   const page = await browser.newPage();
-  console.log("---1.1")
+  console.log("---1.1");
 
   await page.goto(`file://${inputFile}`, {
-    waitUntil: 'networkidle0'
+    waitUntil: "networkidle0",
   });
-  console.log("---2")
+  console.log("---2");
 
   await page.pdf({
     path: outputFile,
-    format: 'A4',
+    format: "A4",
     border: 0,
     margin: {
-      top: '2cm',
-      right: '2cm',
-      bottom: '2cm',
-      left: '2.54cm',
+      top: "2cm",
+      right: "2cm",
+      bottom: "2cm",
+      left: "2.54cm",
     },
   });
-  
-  console.log("---3")
+
+  console.log("---3");
   await browser.close();
-  console.log("---4")
+  console.log("---4");
+  sync = false;
 };
 
 // Build PDF
-buildPdf(`${outputDir}/index.html`, `${outputDir}/${pdfFileName}`).catch(console.error);
-
+buildPdf(`${outputDir}/index.html`, `${outputDir}/${pdfFileName}`).catch(
+  console.error
+);
+while (sync) {
+  require("deasync").sleep(100);
+}
